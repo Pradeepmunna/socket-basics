5f462cfbbcd9d1cd500bb8aa7f6b01089d83e21b
Pradeep <here.pradeepa@gmail.com>
2018-09-06 20:48:47 +0530

    add front end design to message

--- public/js/app.js
+++ public/js/app.js
@@ -5,6 +5,20 @@ socket.on('connect', function() {
 });
 
 socket.on('message', function(message) {
-	console.log('new message');
+	console.log('New message: ');
 	console.log(message.text);
+});
+
+//submiting new message
+var $form = jQuery('#message-form');
+
+$form.on('submit', function(event) {
+	event.preventDefault();
+	var $message=$form.find('input[name=message]');
+	socket.emit('message', {
+		text: $message.val()
+		
+
+	});
+	$message.val('');
 });
