e437cd4609da5783d0f8a38f531b4b9a0ad03c17
Pradeep <here.pradeepa@gmail.com>
2018-09-06 16:02:44 +0530

    added coustom message event

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
-var PORT = process.env.PORT||3000;
+var PORT = process.env.PORT || 3000;
 var express = require('express');
-var app =express();
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-app.use(express.static(__dirname+'/public'));
-io.on('connection',function(){
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', function(socket) {
 	console.log('user connected via socket.io');
-}
-);
-http.listen(PORT, function () {
+	socket.on('message', function(message) {
+		console.log('message received :' + message.text);
+	socket.broadcast.emit('message',message);
+	});
+	socket.emit('message', {
+		text: 'welcome to the chat application'
+	});
+});
+
+http.listen(PORT, function() {
 	console.log('Server Started')
 });
